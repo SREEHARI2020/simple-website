6498393bcbb285c22eae761c1d61e9773fe122ca
SREEHARI <sreehari2610@gmail.com>
2020-11-24 15:49:29 +0530

    initial setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Navbar = () => {
+    return (
+        <div className="nav">
+            <div className="container">
+                <h1>React blog</h1>
+                
+            </div>
+        </div>
+    )
+}
